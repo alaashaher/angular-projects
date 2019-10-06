277b0a68b188e39d817145c77cf79d1dd361ae22
mahmoudsh47@gmail.com <alaashaher12@gmail.com>
2019-10-06 14:18:07 +0200

    create components

---
+++ shopping-app/src/app/recipes/recipe-detailes/recipe-detailes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detailes',
+  templateUrl: './recipe-detailes.component.html',
+  styleUrls: ['./recipe-detailes.component.scss']
+})
+export class RecipeDetailesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
